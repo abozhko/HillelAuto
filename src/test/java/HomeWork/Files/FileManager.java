cca7bd7c83bf315e67ba87d3cebc01eefb8d6cc4
Anatoliy_Bozhko <abozhko@intersog.com>
2016-03-31 18:11:36 +0300

    FileManager_added

--- src/test/java/HomeWork/Files/FileManager.java
+++ src/test/java/HomeWork/Files/FileManager.java
@@ -1,32 +1,53 @@
 package HomeWork.Files;
 
-import java.io.*;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 
 /**
  * Created by Papko on 28.03.2016.
  */
 public class FileManager implements Items {
+    String name;
 
-    public FileManager() {
+    public FileManager(String fileName) {
+        name = fileName;
     }
 
-    public void create (String fileName){
+    public void create() {
         try {
-            File file = new File(PATH+"Test2.txt");
+            System.out.println("Creating the txt file...\n");
+            File file = new File(PATH + name + ".txt");
             FileWriter fileWriter = new FileWriter(file);
             for(int i=0; i<=300; i++){
-                fileWriter.write("\n"+fileName+i);
+                fileWriter.write("\n" + name + i + " \r\n");
 
             }
             fileWriter.flush();
             fileWriter.close();
+            System.out.println("\nThe file '" + name + ".txt' created.");
 
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
-    public void delete (String file){}
-    public void rename (String file){}
-    public void copy (String file){}
+
+    public void delete() {
+        System.out.println("Deleting the last file...\n");
+        File file = new File(PATH + name + ".txt");
+        file.delete();
+        System.out.println("\nThe file '" + name + ".txt' deleted.");
+    }
+
+    public void rename(String newName) {
+        File file = new File(PATH + name + ".txt");
+        file.renameTo(new File(PATH + newName + ".txt"));
+        System.out.println("\nFile " + name + ".txt successfully renamed to " + newName + ".txt\n");
+        name = newName;
+    }
+
+    public void copy() {
+        System.out.println("Feature in progres...");
+    }
 
 }
--- src/test/java/HomeWork/Files/Menu.java
+++ src/test/java/HomeWork/Files/Menu.java
@@ -1,20 +1,63 @@
 package HomeWork.Files;
 
+import java.util.Scanner;
+
 /**
  * Created by Papko on 28.03.2016.
  */
 public class Menu {
     public static void main(String[] args){
-        System.out.println();
-        System.out.println("1. Create file.");
-        System.out.println("2. Delete file.");
-        System.out.println("3. Rename file.");
-        System.out.println("4. Copy file.");
-
+        /* //Example from lesson
         FileManager fileManager = new FileManager();
-        fileManager.create("Test.txt");
+        fileManager.create("Test.txt");*/
 
-        //SQLManager sqlManager = new SQLManager();
-       // sqlManager.executeSQL("Select * from ec_audit");
+        int choice;
+        String fileName;
+
+        System.out.println("Input filename: ");
+        Scanner inputName = new Scanner(System.in);
+        fileName = inputName.nextLine();
+        FileManager fileManager = new FileManager(fileName);
+
+        Scanner in = new Scanner(System.in);
+        do {
+            do {
+                String menu = "\n Please, make our choice:"
+                        + "\n(1) Create a txt file."
+                        + "\n(2) Delete file."
+                        + "\n(3) Rename file."
+                        + "\n(4) Copy file."
+                        + "\n(0) Exit Program";
+                System.out.println(menu);
+                choice = in.nextInt();
+            }
+            while (choice < 0 || choice > 4); // This will make the menu repeat if option is higher than 4 or lowen than 0.
+
+            switch (choice) {
+                case 1:
+                    fileManager.create();
+                    break;
+                case 2:
+                    fileManager.delete();
+                    break;
+                case 3:
+                    System.out.println("Input new filename: ");
+                    String newName;
+                    Scanner inputNewName = new Scanner(System.in);
+                    newName = inputNewName.nextLine();
+                    fileManager.rename(newName);
+                    break;
+                case 4:
+                    fileManager.copy();
+                    break;
+                default:
+                    System.out.println("This is The End");
+                    break;
+            }
+        } while (choice != 0);
+
+        // Example from lesson
+        /* SQLManager sqlManager = new SQLManager();
+            sqlManager.executeSQL("Select * from ec_audit");*/
     }
 }
